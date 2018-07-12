4323198cb8ef8b00ba4d0e8368716e18e009b50f
atomat <atomac@163.com>
2018-07-12 23:12:01 +0800

    1.2

--- src/main/java/com/hcb168/slowdfs/client/ClientUtil.java
+++ src/main/java/com/hcb168/slowdfs/client/ClientUtil.java
@@ -365,15 +365,4 @@ public class ClientUtil {
 	public static void deleteFile(String slowdfsHost, String groupId, String fileId) throws Exception {
 		deleteFile(slowdfsHost, "/slowdfs", groupId, fileId);
 	}
-
-	public static void main(String[] args) throws Exception {
-		String[] hosts = new String[] { "http://127.0.0.1:8080", "http://127.0.0.1:18080" };
-		Map<String, Object> map = fileUploadToHosts(hosts, "public", "E:\\tmp/summer.zip", "summer.zip");
-		System.out.println(map);
-
-		ClientUtil.fileDownloadFromHosts(hosts, "/download/public/3dbf02307eda03225124c0c324075cec.zip",
-				"e:/tmp/abc.zip");
-
-		deleteFile(hosts[1], "/slowdfs", "public", "3dbf02307eda03225124c0c324075cec");
-	}
 }
